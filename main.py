5c1bbdceaa6f55cf28213c5766c34627fc0062d5
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-21 12:17:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,134 +1,126 @@
-import json 
+import json
 
-with open("file.json", 'r', encoding='utf-8') as file: 
-    file = json.load(file) 
-dizel = True
-counter = 0 
-
-while True:
+def menu():
     print("""
         1 - Вывести все записи 
         2 - Вывести запись по полю 
         3 - Добавить запись 
         4 - Удалить запись по полю 
         5 - Выйти из программы
+    """)
+
+def all_see(i_file):
+    for i in i_file:
+        print(f"""
+        Код: {i['id']}, 
+        Имя: {i['name']},                       
+        Производитель: {i['manufacturer']}, 
+        Бензин: {i['is_petrol']},    
+        Объем v3: {i['tank_volume']} 
         """)
 
+def kluch_see(i_file):
     try:
-        point = int(input("Введите действие: "))
+        idnum = int(input("Введите номер машины: "))
     except ValueError:
-        print("Ошибка: Пожалуйста, введите число от 1 до 5.")
-        continue  
+        print("Ошибка: Пожалуйста, введите корректный номер машины.")
+        return
 
-    #Вывод всех записей
-    if point == 1:
-        for i in file:
+    for i in i_file:
+        if idnum == i['id']:
             print(f"""
             Код: {i['id']}, 
             Имя: {i['name']},                       
             Фабрика: {i['manufacturer']}, 
             Заправлена: {i['is_petrol']},    
-            Объем Бака: {i['tank_volume']} 
+            Объем Бака: {i['tank_volume']}
             """)
-        counter += 1
+            return
+    print("Запись не найдена.")
 
-    #Вывод по айди записи
-    elif point == 2:
-        try:
-            idnum = int(input("Введите номер машины: "))
-        except ValueError:
-            print("Ошибка: Пожалуйста, введите корректный номер машины.")
-            continue
+def add_see(i_file):
+    try:
+        ids = int(input("Введите номер машины: "))
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите корректный номер машины.")
+        return
 
-        qwe = False  
-        index = 0  
-        for i in file:
-            if idnum == i['id']:
-                print(f"""
-                Код: {i['id']}, 
-                Имя: {i['name']},                       
-                Фабрика: {i['manufacturer']}, 
-                Заправлена: {i['is_petrol']},    
-                Объем Бака: {i['tank_volume']}
-                Индекс в списке: {index}
-                """)
-                qwe = True  
-                break  
-            index += 1
-        counter += 1
-        if not qwe:
-            print("Запись не найдена.")
+    if any(i['id'] == ids for i in i_file):
+        print("Ошибка: машина с таким номером уже существует.")
+        return
 
-    #Ввод с клавиатуры машины 
-    elif point == 3:
-        try:
-            ids = int(input("Введите номер машины: "))
-        except ValueError:
-            print("Ошибка: Пожалуйста, введите корректный номер машины.")
-            continue
+    name = input("Введите имя машины: ")  
+    manufacturer = input("Введите завод изготовитель: ")  
+    is_petrol = input("Бензин? Введите да/нет: ") 
+    tank_volume = input("Введите объем бака машины: ")  
 
-        errrror = False
-        for i in file:
-            if i['id'] == ids:
-                errrror = True
-                break
-        
-        if errrror:
-            print("Ошибка: машина с таким номером уже существует.")
-        else:
-            name = input("Введите имя машины: ")  
-            manufacturer = input("Введите завод изготовитель: ")  
-            is_petrol = input("бензин ? введите да/нет: ") 
-            tank_volume = input("Введите объем бака машины: ")  
+    try:
+        tank_volume = float(tank_volume)
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите корректный объем бака.")
+        return
 
-            try:
-                tank_volume = float(tank_volume)
-            except ValueError:
-                print("Ошибка: Пожалуйста, введите корректный объем бака.")
-                continue
+    new_i = {
+        'id': ids,
+        'name': name,
+        'manufacturer': manufacturer,
+        'is_petrol': is_petrol.lower() == 'да', 
+        'tank_volume': tank_volume
+    }
 
-            new_i = {
-                'id': ids,
-                'name': name,
-                'manufacturer': manufacturer,
-                'is_petrol': True if is_petrol.lower() == 'да' else False, 
-                'tank_volume': tank_volume
-            }
+    i_file.append(new_i) 
+    with open("file.json", 'w', encoding='utf-8') as output_file: 
+        json.dump(i_file, output_file, ensure_ascii=False, indent=2)
+    print("Машина успешно добавлена.")
 
-            file.append(new_i) 
-            with open("file.json", 'w', encoding='utf-8') as output_file: 
-                json.dump(file, output_file, ensure_ascii=False, indent=2)
-            print("Машина успешно добавлена.")
-        counter += 1
+def del_kluch_see(i_file):
+    try:
+        iddel = int(input("Введите номер машины: "))
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите корректный номер машины.")
+        return
 
-    #Удаление машины
-    elif point == 4:
-        try:
-            iddel = int(input("Введите номер машины: "))
-        except ValueError:
-            print("Ошибка: Пожалуйста, введите корректный номер машины.")
-            continue
-
-        qwe = False  
-
-        for i in file:
-            if iddel == i['id']:
-                file.remove(i)  
-                qwe = True  
-                break 
-
-        if not qwe:
-            print("Запись не найдена.")
-        else:
+    for i in i_file:
+        if iddel == i['id']:
+            i_file.remove(i)  
             with open("file.json", 'w', encoding='utf-8') as output_file:
-                json.dump(file, output_file, ensure_ascii=False, indent=2)
+                json.dump(i_file, output_file, ensure_ascii=False, indent=2)
             print("Машина успешно удалена.")
+            return
+
+    print("Запись не найдена.")
+
+def end_see(counter):
+    print(f"Программа завершена. ОНА МУЧИЛАСЬ РОВНО {counter} РАЗ !!!!!!!!!")
+
+def main():
+    with open("file.json", 'r', encoding='utf-8') as file:
+        i_file = json.load(file)
+
+    counter = 0
+    
+    while True:
+        menu()
+        try:
+            user_input = int(input("Введите действие какое хотите выполнить: "))
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите число от 1 до 5.")
+            continue
+
+        if user_input == 1:
+            all_see(i_file)
+        elif user_input == 2:
+            kluch_see(i_file)
+        elif user_input == 3:
+            add_see(i_file)
+        elif user_input == 4:
+            del_kluch_see(i_file)
+        elif user_input == 5:
+            end_see(counter)
+            break
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите номер от 1 до 5.")
+
         counter += 1
 
-    # Выход из программы
-    elif point == 5:
-        print(f"Программа завершена. ОНА МУЧИЛАСЬ РОВНО {counter} РАЗ !!!!!!!!!")
-        break  
-
-    else:
-        print("Некорректный ввод. Пожалуйста, введите число от 1 до 5.")
+main()
